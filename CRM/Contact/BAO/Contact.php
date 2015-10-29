14274eaf7046beab4950e4e1720bf91392abafac
Adam Roses Wight <awight@wikimedia.org>
2015-10-29 08:07:22 -0700

    Don't create SQL function at runtime
    This breaks the upgrade.
    
    Bug: T117044
    Change-Id: I438be42bf24724b757c5ab76cbb8c89ae6686935

--- CRM/Contact/BAO/Contact.php
+++ CRM/Contact/BAO/Contact.php
@@ -3274,8 +3274,11 @@ LEFT JOIN civicrm_address add2 ON ( add1.master_id = add2.id )
     // Update phone table to populate phone_numeric field
     if (!$tableName || $tableName == 'civicrm_phone') {
       // Define stored sql function needed for phones
-      CRM_Core_DAO::executeQuery(self::DROP_STRIP_FUNCTION_43);
-      CRM_Core_DAO::executeQuery(self::CREATE_STRIP_FUNCTION_43);
+      //
+      // FIXME: WMF bug T117044, we can't create this function at runtime (unknown why not).
+      // CRM_Core_DAO::executeQuery(self::DROP_STRIP_FUNCTION_43);
+      // CRM_Core_DAO::executeQuery(self::CREATE_STRIP_FUNCTION_43);
+
       $info[] = array(
         'table' => array('civicrm_phone'),
         'when' => 'BEFORE',
