c562a493cca3792a6dc9113c158ad1786be6e371
eileen <emcnaughton@wikimedia.org>
2015-11-03 17:05:41 +1300

    Fix slow search on transaction ID (remove wildcard)
    Bug: T117492
    Change-Id: Ie4ea1295205e8dd57ffb8aca1cfc9cfbb7160869

--- CRM/Contribute/Form/Search.php
+++ CRM/Contribute/Form/Search.php
@@ -269,7 +269,7 @@ class CRM_Contribute_Form_Search extends CRM_Core_Form_Search {
         'contribution_soft_credit_type_id',
         'contribution_status_id',
         'contribution_source',
-        'contribution_trxn_id',
+        //'contribution_trxn_id',
         'invoice_id',
       );
       foreach ($specialParams as $element) {
