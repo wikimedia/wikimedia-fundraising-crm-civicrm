eccd720ea1571bfe4f471d72ac7336336ea7981e
eileen <emcnaughton@wikimedia.org>
2015-11-20 14:28:35 +1300

    fix missing arg on upgrade
    Bug: T116877
    Change-Id: I3721772f30cf39d0255f4c53df99a59b7ce46c8f

--- CRM/Core/BAO/Navigation.php
+++ CRM/Core/BAO/Navigation.php
@@ -903,7 +903,7 @@ ORDER BY parent_id, weight";
     // Create or update the All Reports link.
     self::createOrUpdateReportNavItem('All Reports', 'civicrm/report/list', 'reset=1', $reports_nav->id, 'access CiviReport', $domain_id);
     // Create or update the My Reports link.
-    self::createOrUpdateReportNavItem('My Reports', 'civicrm/report/list', 'myreports=1&reset=1', $reports_nav->id, 'access CiviReport');
+    self::createOrUpdateReportNavItem('My Reports', 'civicrm/report/list', 'myreports=1&reset=1', $reports_nav->id, 'access CiviReport', $domain_id);
   }
 
   /**
