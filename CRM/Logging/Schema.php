2230a8427b7ae1a33c9cc6037ae48dfe3735267e
eileen <emcnaughton@wikimedia.org>
2016-03-09 16:37:34 +1300

    Add tests for logging functionality to ensure the tables are still creating well
    The tests are actually being added into Core - this is a backport to keep the file in sync
    
    https://github.com/civicrm/civicrm-core/pull/7988/commits
    
    Bug: T130161
    
    Change-Id: I6ef4d48d16e25d232b0f46e7ce3cf5d569e7053c

--- CRM/Logging/Schema.php
+++ CRM/Logging/Schema.php
@@ -625,9 +625,7 @@ COLS;
    * Predicate whether logging is enabled.
    */
   public function isEnabled() {
-    $config = CRM_Core_Config::singleton();
-
-    if ($config->logging) {
+    if (CRM_Core_Config::singleton()->logging) {
       return $this->tablesExist() and $this->triggersExist();
     }
     return FALSE;
@@ -638,6 +636,19 @@ COLS;
    */
   private function tablesExist() {
     return !empty($this->logs);
+  }
+
+  /**
+   * Drop all log tables.
+   *
+   * This does not currently have a usage outside the tests.
+   */
+  public function dropAllLogTables() {
+    if ($this->tablesExist()) {
+      foreach ($this->logs as $log_table) {
+        CRM_Core_DAO::executeQuery("DROP TABLE $log_table");
+      }
+    }
   }
 
   /**
