8d800cbdf3a146631d456c4861be166bf570e020
eileen <emcnaughton@wikimedia.org>
2017-07-06 09:51:54 +1200

    Remove commands that succeeded from upgrade.
    I disconnected. It did these commands. To rekick into gear need
    to also do
    
    UPDATE civicrm_domain SET version = '4.7.15';
    TRUNCATE civicrm_queue_item;
    
    Change-Id: I12c1b17ebcdcf8c44726baa2b0b5b249eac53f6c

--- CRM/Upgrade/Incremental/php/FourSeven.php
+++ CRM/Upgrade/Incremental/php/FourSeven.php
@@ -303,15 +303,6 @@ class CRM_Upgrade_Incremental_php_FourSeven extends CRM_Upgrade_Incremental_Base
    * @param string $rev
    */
   public function upgrade_4_7_16($rev) {
-    $this->addTask('CRM-19723 - Add icon column to civicrm_option_value', 'addColumn',
-      'civicrm_option_value', 'icon', "varchar(255) COMMENT 'crm-i icon class' DEFAULT NULL");
-    $this->addTask('CRM-19769 - Add color column to civicrm_tag', 'addColumn',
-      'civicrm_tag', 'color', "varchar(255) COMMENT 'Hex color value e.g. #ffffff' DEFAULT NULL");
-    $this->addTask('CRM-19779 - Add color column to civicrm_option_value', 'addColumn',
-      'civicrm_option_value', 'color', "varchar(255) COMMENT 'Hex color value e.g. #ffffff' DEFAULT NULL");
-    $this->addTask('Add new CiviMail fields', 'addMailingTemplateType');
-    $this->addTask('CRM-19770 - Add is_star column to civicrm_activity', 'addColumn',
-      'civicrm_activity', 'is_star', "tinyint DEFAULT '0' COMMENT 'Activity marked as favorite.'");
     $this->addTask(ts('Upgrade DB to %1: SQL', array(1 => $rev)), 'runSql', $rev);
   }
 
