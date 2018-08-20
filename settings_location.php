17f68950561b6a9c238938589cb02a22d35cc881
eileen <emcnaughton@wikimedia.org>
2018-08-21 09:46:43 +1200

    Fix path in settings_location.php.
    This got altered back in March to be less server specific, but also broken :-(
    
    CkFinder relies on this when uploading images. It's a known pain point & I have
    put some time before into making it more robust but not succeeded.
    
    On staging with this patch I get success on upload to server but not on browse server
    but I think that is part of the bigger issue & this fixes as far as it goes
    
    Bug: T202330
    
    Change-Id: I76c227e61d2b86037195f5742d6e02bb7fff63f5

--- settings_location.php
+++ settings_location.php
@@ -4,5 +4,5 @@
  * Used where CiviCRM is part of an install profile like CiviCRM Starterkit.
  */
 if (!defined('CIVICRM_CONFDIR')) {
-  define( 'CIVICRM_CONFDIR', realpath( dirname( __FILE__ ) . "/../../../default" ));
+  define( 'CIVICRM_CONFDIR', realpath( dirname( __FILE__ )) . "/../drupal/sites/default" );
 }
